aea1727fc9834b9766b761c5266ea35185293588
longshaww <at400123@gmail.com>
2022-04-05 01:56:51 +0700

    unified

--- server/src/apartment/dto/create-apartment.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MaxLength } from 'class-validator';
-
-export class CreateApartmentDto {
-  @ApiProperty()
-  @IsString()
-  @MaxLength(20)
-  name: string;
-}
--- server/src/lessor/dto/update-lessor.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateLessorDto } from './create-lessor.dto';
-
-export class UpdateLessorDto extends PartialType(CreateLessorDto) {}
