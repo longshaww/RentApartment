ddb1e4f96c7ef604c9b75ce9d4e9472e34c3cf2d
longshaww <at400123@gmail.com>
2022-05-25 08:30:00 +0700

    dev

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
-  app.enableCors({
-    origin: configService.get('FRONTEND_URL'),
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: configService.get('FRONTEND_URL'),
+  //   credentials: true,
+  // });
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
